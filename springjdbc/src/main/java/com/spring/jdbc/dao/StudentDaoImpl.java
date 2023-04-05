b5122c4b8765aa14617aa8eba7b8483da72527a6
Pratibhasharma24 <ps2121@checkboxtechnology.com>
2023-04-05 12:18:29 +0530

    Jdbcconcept

--- springjdbc/src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
+++ springjdbc/src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
@@ -2,12 +2,17 @@ package com.spring.jdbc.dao;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import com.spring.jdbc.entities.Student;
 
+@Component("studentDao")
 public class StudentDaoImpl implements StudentDao {
+	
+	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
 	public int insert(Student student) {
@@ -50,7 +55,7 @@ public class StudentDaoImpl implements StudentDao {
 	public JdbcTemplate getJdbcTemplate() {
 		return jdbcTemplate;
 	}
-
+	
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
