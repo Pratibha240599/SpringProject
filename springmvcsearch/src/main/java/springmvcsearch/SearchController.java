f695bfc278300518bd2aaa74ed790ade23b93d42
Pratibhasharma24 <ps2121@checkboxtechnology.com>
2023-04-28 18:36:33 +0530

    SpringMVCsearch

---
+++ springmvcsearch/src/main/java/springmvcsearch/SearchController.java
@@ -0,0 +1,32 @@
+package springmvcsearch;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SearchController {
+	
+	@RequestMapping("/home")
+	public String home()
+	{
+		System.out.println("going to the home view..");
+		return "home";
+	}
+	
+	@RequestMapping("/search")
+	public RedirectView search(@RequestParam("querybox") String query)
+	{
+		if(query.isBlank())
+		{
+			RedirectView redirectView = new RedirectView();
+			//redirectView.setUrl("search");
+			return redirectView;
+		}
+		String url = "https://www.google.com/search?q=" + query;
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl(url);
+		return redirectView;
+	}
+}
