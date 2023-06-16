f4ed66f4c6767c57ac174dba3a8ff478522c1a4d
Pratibhasharma24 <ps2121@checkboxtechnology.com>
2023-06-16 14:50:36 +0530

    springbootproject

--- springbootproject/src/main/java/com/springboot/mvc/dao/UserRepository.java
+++ springbootproject/src/main/java/com/springboot/mvc/dao/UserRepository.java
@@ -4,6 +4,6 @@ import com.springboot.mvc.entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface UserRepository extends CrudRepository<User, Integer> {
 }
