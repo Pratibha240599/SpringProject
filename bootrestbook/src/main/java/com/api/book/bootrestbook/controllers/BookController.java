ff7c5112f15f797a6297a0ebfd13c6e407b88e90
Pratibhasharma24 <ps2121@checkboxtechnology.com>
2023-06-26 18:33:38 +0530

    Springbootrestapi

--- bootrestbook/src/main/java/com/api/book/bootrestbook/controllers/BookController.java
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/controllers/BookController.java
@@ -13,15 +13,42 @@ public class BookController {
 
     @Autowired
     private BookService bookService;
+
+    //get all book handler
     @GetMapping("/books")
     public List<Book> getBooks()
     {
         return this.bookService.getAllBooks();
     }
 
+    //get single book handler
     @GetMapping("/books/{id}")
     public Book getBook(@PathVariable("id") int id)
     {
         return bookService.getBookById(id);
+    }
+
+    //new book handler
+    @PostMapping("/books")
+    public Book addBook(@RequestBody Book book)
+    {
+        Book b = this.bookService.addBook(book);
+        System.out.println(book);
+        return b;
+    }
+
+    //delete book handler
+    @DeleteMapping("/books/{bookId}")
+    public void deleteBook(@PathVariable("bookId") int bookId)
+    {
+        this.bookService.deleteBook(bookId);
+    }
+
+    //update book handler
+    @PutMapping("/books/{bookId}")
+    public Book updateBook(@RequestBody Book book, @PathVariable("bookId") int bookId)
+    {
+        this.bookService.updateBook(book,bookId);
+        return book;
     }
 }
--- bootrestbook/src/main/java/com/api/book/bootrestbook/services/BookService.java
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/services/BookService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class BookService {
@@ -29,5 +30,31 @@ public class BookService {
         Book book = null;
         book = list.stream().filter(e->e.getId()==id).findFirst().get();
         return book;
+    }
+
+    //adding the book
+    public Book addBook(Book b)
+    {
+        list.add(b);
+        return b;
+    }
+
+    //delete book
+    public void deleteBook(int bid)
+    {
+        list = list.stream().filter(book -> book.getId()!=bid).collect(Collectors.toList());
+    }
+
+    //update the book
+    public void updateBook(Book book,int bookId)
+    {
+        list = list.stream().map(b->{
+            if(b.getId()==bookId)
+            {
+                b.setTitle(book.getTitle());
+                b.setAuthor(book.getAuthor());
+            }
+            return b;
+        }).collect(Collectors.toList());
     }
 }
