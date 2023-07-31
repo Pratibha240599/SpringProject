14c1132dd09afa3719b9b991beaf895daf4c203c
Pratibhasharma24 <ps2121@checkboxtechnology.com>
2023-07-31 18:34:06 +0530

    Smartcontactmanager

---
+++ smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.smart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
+import com.smart.entities.User;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test()
+    {
+        User user = new User();
+        user.setName("Pratibha Sharma");
+        user.setEmail("ps@dev.io");
+
+        Contact contact = new Contact();
+        user.getContacts().add(contact);
+        userRepository.save(user);
+
+        return "Working";
+    }
+    
+}
