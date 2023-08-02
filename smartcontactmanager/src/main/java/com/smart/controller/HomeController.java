90374df44d55f5b974aecf88ef8493d20e5397d6
Pratibhasharma24 <ps2121@checkboxtechnology.com>
2023-08-02 18:29:29 +0530

    Smartcontactmanager

--- smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
@@ -1,13 +1,24 @@
 package com.smart.controller;
 
+import java.security.PublicKey;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.smart.dao.UserRepository;
-import com.smart.entities.Contact;
 import com.smart.entities.User;
+import com.smart.helper.Message;
+import com.smart.dao.UserRepository;
+//import com.smart.entities.Contact;
+//import com.smart.entities.User;
 
 @Controller
 public class HomeController {
@@ -15,19 +26,71 @@ public class HomeController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/test")
+    /*@GetMapping("/test")
     @ResponseBody
     public String test()
     {
         User user = new User();
         user.setName("Pratibha Sharma");
-        user.setEmail("ps@dev.io");
+        user.setEmail("ps12@gmail.com");
 
         Contact contact = new Contact();
         user.getContacts().add(contact);
         userRepository.save(user);
 
         return "Working";
+    }*/
+
+    @RequestMapping("/")
+    public String home(Model model)
+    {
+        model.addAttribute("title", "Home - Smart Contact Manager");
+        return "home";
+    }
+
+    @RequestMapping("/about")
+    public String about(Model model)
+    {
+        model.addAttribute("title", "About - Smart Contact Manager");
+        return "about";
+    }
+
+    @RequestMapping("/signup")
+    public String signup(Model model)
+    {
+        model.addAttribute("title", "Register - Smart Contact Manager");
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+    //handler for registering user
+    @RequestMapping(value = "do_register", method = RequestMethod.POST)
+    public String registerUser(@ModelAttribute("user") User user, @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model, HttpSession session)
+    {
+        try {
+            if(!agreement)
+        {
+            System.out.println("You have not agreed the terms and conditions");
+            throw new Exception("You have not agreed the terms and conditions");
+        }
+        user.setRole("ROLE_USER");
+        user.setEnabled(true);
+        user.setImageUrl("default.png");
+        System.out.println("Agreement "+agreement);
+        System.out.println("USER "+user);
+
+        User result = this.userRepository.save(user);
+        model.addAttribute("user", new User());
+        session.setAttribute("message", new Message("Successfully Registered !!", "alert-success"));
+        return "signup";
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("user", user);
+            session.setAttribute("message", new Message("Something Went wrong !!"+e.getMessage(), "alert-danger"));
+            return "signup";
+        }
+        
     }
     
 }
