4a8a6eef46a8d9001c9e4df73fabf6717b6030a6
Pratibhasharma24 <ps2121@checkboxtechnology.com>
2023-08-03 18:29:35 +0530

    Smartcontactmanager

--- smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
@@ -3,10 +3,12 @@ package com.smart.controller;
 import java.security.PublicKey;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -65,13 +67,19 @@ public class HomeController {
 
     //handler for registering user
     @RequestMapping(value = "do_register", method = RequestMethod.POST)
-    public String registerUser(@ModelAttribute("user") User user, @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model, HttpSession session)
+    public String registerUser(@Valid @ModelAttribute("user") User user,BindingResult result1, @RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model, HttpSession session)
     {
         try {
             if(!agreement)
         {
             System.out.println("You have not agreed the terms and conditions");
             throw new Exception("You have not agreed the terms and conditions");
+        }
+        if(result1.hasErrors())
+        {
+            System.out.println("ERROR " + result1.toString());
+            model.addAttribute("user", user);
+            return "signup";
         }
         user.setRole("ROLE_USER");
         user.setEnabled(true);
--- smartcontactmanager/src/main/java/com/smart/entities/User.java
+++ smartcontactmanager/src/main/java/com/smart/entities/User.java
@@ -12,6 +12,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 
 @Entity
@@ -21,6 +23,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @NotBlank(message = "Name field is required !!")
+    @Size(min = 2,max = 20,message = "min 2 and max 20 characters are allowed !!")
     private String name;
 
     @Column(unique = true)
